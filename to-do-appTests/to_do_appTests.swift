ad08804827455d7a2c4f82affcc7528a6392664e
jesusmaldonado <jesusmaldonado317@gmail.com>
2015-09-22 12:51:02 -0400

    first commit

---
+++ to-do-appTests/to_do_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  to_do_appTests.swift
+//  to-do-appTests
+//
+//  Created by Jesús Maldonado on 9/20/15.
+//  Copyright © 2015 Jesus Maldonado. All rights reserved.
+//
+
+import XCTest
+@testable import to_do_app
+
+class to_do_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ todo app/todo app/FirstViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FirstViewController.swift
+//  todo app
+//
+//  Created by Jesús Maldonado on 9/20/15.
+//  Copyright © 2015 Jesus Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.itemToAdd.delegate = self
+        if (!(NSUserDefaults.standardUserDefaults().objectForKey("array") != nil)) {
+            let array = []
+            NSUserDefaults.standardUserDefaults().setObject(array, forKey: "array")
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet var itemToAdd: UITextField!
+    
+    @IBAction func addToStorare(sender: AnyObject) {
+        var array = NSUserDefaults.standardUserDefaults().objectForKey("array") as AnyObject! as! Array<String>
+        print(array)
+        array.append(itemToAdd.text!)
+        NSUserDefaults.standardUserDefaults().setObject(array, forKey: "array")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
