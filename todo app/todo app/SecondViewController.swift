c46f501407a6504f92606b062c59b2427bf9dfd3
jesusmaldonado <jesusmaldonado317@gmail.com>
2015-09-22 20:20:09 -0400

    adds delete functionality

--- todo app/todo app/SecondViewController.swift
+++ todo app/todo app/SecondViewController.swift
@@ -8,12 +8,63 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.allowsMultipleSelectionDuringEditing = false;
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let arrayHasBeenSet = NSUserDefaults.standardUserDefaults().objectForKey("array")
+        if (arrayHasBeenSet != nil) {
+            let array = NSUserDefaults.standardUserDefaults().objectForKey("array") as AnyObject! as! Array<String>
+            return array.count
+        } else {
+            return 0
+        }
+    }
+
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        let array = NSUserDefaults.standardUserDefaults().objectForKey("array") as AnyObject! as! Array<String>
+//
+        cell.textLabel?.text = "\(array[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true;
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        var array = NSUserDefaults.standardUserDefaults().objectForKey("array") as AnyObject! as! Array<String>
+        if (editingStyle == UITableViewCellEditingStyle.Delete){
+            array.removeAtIndex(indexPath.row)
+            tableView.beginUpdates()
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+                NSUserDefaults.standardUserDefaults().setObject(array, forKey: "array")
+            tableView.endUpdates()
+
+        }
+
+    
+    }
+    
+    
+    func deleteRowsAtIndexPaths(indexPaths: [NSIndexPath], withRowAnimation animation: UITableViewRowAnimation) {
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
